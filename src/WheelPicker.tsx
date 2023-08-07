1738d4a2a50b2d8ec37d78a67ed71e0fafa3fe6b
danielnedefo <danilka4412@gmail.com>
2023-08-07 11:11:47 +0300

    Revert

--- src/WheelPicker.tsx
+++ src/WheelPicker.tsx
@@ -23,7 +23,6 @@ interface Props {
   secondaryTextStyle?: TextStyle;
   itemStyle?: ViewStyle;
   itemHeight?: number;
-  maxIdx: number;
   containerStyle?: ViewStyle;
   containerProps?: Omit<ViewProps, 'style'>;
   scaleFunction?: (x: number) => number;
@@ -38,7 +37,6 @@ const WheelPicker: React.FC<Props> = ({
   selectedIndex,
   options,
   onChange,
-  maxIdx,
   selectedIndicatorStyle = {},
   containerStyle = {},
   itemStyle = {},
@@ -153,25 +151,22 @@ const WheelPicker: React.FC<Props> = ({
         })}
         data={paddedOptions}
         keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item: option, index }) => {
-          if (index > maxIdx) return <></>;
-          return (
-            <WheelPickerItem
-              key={`option-${index}`}
-              index={index}
-              option={option}
-              style={itemStyle}
-              textStyle={itemTextStyle}
-              secondaryTextStyle={secondaryTextStyle}
-              height={itemHeight}
-              currentScrollIndex={currentScrollIndex}
-              scaleFunction={scaleFunction}
-              rotationFunction={rotationFunction}
-              opacityFunction={opacityFunction}
-              visibleRest={visibleRest}
-            />
-          )
-        }}
+        renderItem={({ item: option, index }) => (
+          <WheelPickerItem
+            key={`option-${index}`}
+            index={index}
+            option={option}
+            style={itemStyle}
+            textStyle={itemTextStyle}
+            secondaryTextStyle={secondaryTextStyle}
+            height={itemHeight}
+            currentScrollIndex={currentScrollIndex}
+            scaleFunction={scaleFunction}
+            rotationFunction={rotationFunction}
+            opacityFunction={opacityFunction}
+            visibleRest={visibleRest}
+          />
+        )}
       />
     </View>
   );
